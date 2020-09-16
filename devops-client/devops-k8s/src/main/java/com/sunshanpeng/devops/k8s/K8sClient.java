b2168fcd8da1e448eb99d022d671196cf67f7413
sunshanpeng <sunshanpeng@outlook.com>
2020-09-16 16:52:36 +0800

    feature:new client from kubeconfigContents

--- devops-client/devops-k8s/src/main/java/com/sunshanpeng/devops/k8s/K8sClient.java
+++ devops-client/devops-k8s/src/main/java/com/sunshanpeng/devops/k8s/K8sClient.java
@@ -1,7 +1,10 @@
 package com.sunshanpeng.devops.k8s;
 
+import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+
+import java.io.IOException;
 
 public class K8sClient {
 
@@ -11,8 +14,8 @@ public class K8sClient {
         client = new DefaultKubernetesClient();
     }
 
-    public K8sClient(String kubeConfig) {
-        client = DefaultKubernetesClient.fromConfig(kubeConfig);
+    public K8sClient(String kubeconfigContents) throws IOException {
+        client = new DefaultKubernetesClient(Config.fromKubeconfig(kubeconfigContents));
     }
 
     public KubernetesClient getClient() {
