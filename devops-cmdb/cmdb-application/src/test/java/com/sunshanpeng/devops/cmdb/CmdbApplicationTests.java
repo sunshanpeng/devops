09706660b64d8e38ee5b375a58e8334b2fe1932a
sunshanpeng <sunshanpeng@outlook.com>
2020-10-01 11:13:06 +0800

    module:add cmdb-application

---
+++ devops-cmdb/cmdb-application/src/test/java/com/sunshanpeng/devops/cmdb/CmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshanpeng.devops.cmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
