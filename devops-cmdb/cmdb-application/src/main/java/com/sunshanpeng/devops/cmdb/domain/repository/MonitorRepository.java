a99e0a49173186e4adc02fbdf15f3e24037d54b0
sunshanpeng <sunshanpeng@outlook.com>
2020-10-20 15:21:07 +0800

    fix:MonitorEntity

--- devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/repository/MonitorRepository.java
+++ devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/repository/MonitorRepository.java
@@ -1,7 +1,7 @@
 package com.sunshanpeng.devops.cmdb.domain.repository;
 
-import com.sunshanpeng.devops.cmdb.domain.entity.CiInfoEntity;
+import com.sunshanpeng.devops.cmdb.domain.entity.MonitorEntity;
 import com.sunshanpeng.devops.common.base.BaseRepository;
 
-public interface MonitorRepository extends BaseRepository<CiInfoEntity, Long> {
+public interface MonitorRepository extends BaseRepository<MonitorEntity, Long> {
 }
