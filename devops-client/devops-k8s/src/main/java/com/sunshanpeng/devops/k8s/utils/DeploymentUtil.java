7d91f0209fb9f1b4b6a358bd662aab2273e69fab
sunshanpeng <sunshanpeng@outlook.com>
2020-11-06 14:35:56 +0800

    feature:更新镜像接口

--- devops-client/devops-k8s/src/main/java/com/sunshanpeng/devops/k8s/utils/DeploymentUtil.java
+++ devops-client/devops-k8s/src/main/java/com/sunshanpeng/devops/k8s/utils/DeploymentUtil.java
@@ -5,10 +5,12 @@ import com.sunshanpeng.devops.k8s.dto.DeploymentDTO;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class DeploymentUtil {
@@ -27,6 +29,18 @@ public class DeploymentUtil {
         }
         return client.apps().deployments().inNamespace(deploymentDTO.getNamespace())
                 .create(deploymentDTO.generate());
+    }
+
+    public static void updateImage(@NotBlank String clusterCode, @NotBlank String namespace,
+                                 @NotBlank String appName, @NotBlank String image) throws IOException {
+        KubernetesClient client = KubeConfig.getClient(clusterCode);
+        Deployment deployment = client.apps().deployments().inNamespace(namespace).withName(appName).get();
+        Assert.notNull(deployment, String.format("deployment of %s/%s is not exist", namespace, appName));
+
+        deployment.getSpec().getTemplate().getSpec().getContainers()
+                .stream().filter(container -> appName.equals(container.getName()))
+                .peek(container -> container.setImage(image)).collect(Collectors.toList());
+        client.apps().deployments().inNamespace(namespace).createOrReplace(deployment);
     }
 
 
