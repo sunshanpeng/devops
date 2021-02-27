a978c00ea40f299db1a1de90728eb0479724317c
sunshanpeng <sunshanpeng@outlook.com>
2021-02-27 22:54:10 +0800

    fix:deploy

--- devops-deploy/deploy-application/src/main/java/com/sunshanpeng/devops/deploy/DeployApplication.java
+++ devops-deploy/deploy-application/src/main/java/com/sunshanpeng/devops/deploy/DeployApplication.java
@@ -1,4 +1,4 @@
-package com.sunshanpeng.devops.message;
+package com.sunshanpeng.devops.deploy;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class MessageApplication {
+public class DeployApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MessageApplication.class, args);
+        SpringApplication.run(DeployApplication.class, args);
     }
 }
