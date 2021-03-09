ead65edaa84bffb6ad71c3f812148a2e05407cb5
sunshanpeng <sunshanpeng@accesscorporate.com.cn>
2021-03-09 20:48:26 +0800

    feature:deploy -> publish

--- devops-publish/publish-application/src/main/java/com/sunshanpeng/devops/publish/PublishApplication.java
+++ devops-publish/publish-application/src/main/java/com/sunshanpeng/devops/publish/PublishApplication.java
@@ -1,4 +1,4 @@
-package com.sunshanpeng.devops.message;
+package com.sunshanpeng.devops.publish;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class MessageApplication {
+public class PublishApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MessageApplication.class, args);
+        SpringApplication.run(PublishApplication.class, args);
     }
 }
