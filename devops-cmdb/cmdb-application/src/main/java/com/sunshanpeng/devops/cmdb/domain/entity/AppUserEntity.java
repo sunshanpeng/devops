87f5b969ccadc2e07188cdb57549cce08f7ce2a2
sunshanpeng <sunshanpeng@outlook.com>
2021-03-14 13:35:02 +0800

    feature:应用负责人信息

--- devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/entity/AppUserEntity.java
+++ devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/entity/AppUserEntity.java
@@ -2,11 +2,13 @@ package com.sunshanpeng.devops.cmdb.domain.entity;
 
 import com.sunshanpeng.devops.common.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -19,15 +21,23 @@ import javax.persistence.Table;
 public class AppUserEntity extends BaseEntity {
     public static final String TABLE_NAME = "t_app_user";
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "应用名", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '应用名'")
     private String appName;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "负责人类型", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '负责人类型'")
     private String AppUserType;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "用户名", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(255) not null default '' comment '用户名'")
     private String username;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "全名", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(255) not null default '' comment '用户名'")
     private String fullName;
 }
