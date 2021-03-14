a681f330833d8a27aff2a4dd9506809ba78661d7
sunshanpeng <sunshanpeng@outlook.com>
2021-03-14 13:16:55 +0800

    feature:重构应用信息

--- devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/entity/AppInfoEntity.java
+++ devops-cmdb/cmdb-application/src/main/java/com/sunshanpeng/devops/cmdb/domain/entity/AppInfoEntity.java
@@ -2,11 +2,14 @@ package com.sunshanpeng.devops.cmdb.domain.entity;
 
 import com.sunshanpeng.devops.common.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -19,30 +22,43 @@ import javax.persistence.Table;
 public class AppInfoEntity extends BaseEntity {
     public static final String TABLE_NAME = "t_app_info";
 
-    @Column(nullable = false, unique = true)
+    @NotBlank
+    @ApiModelProperty(value = "应用名", required = true)
+    @Column(nullable = false, unique = true, columnDefinition = "varchar(64) not null default '' comment '应用名'")
     private String appName;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "应用描述", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(255) not null default '' comment '应用描述'")
     private String appDescription;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "应用类型", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '应用类型'")
     private String appType;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "应用级别", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '应用级别'")
     private String appLevel;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "应用状态", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '应用状态'")
     private String status;
 
-    @Column(nullable = false)
+    @NotNull
+    @ApiModelProperty("应用端口")
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '应用端口'")
     private String ports;
 
-    @Column(nullable = false)
-    private String buCode;
-
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "组织ID", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '组织ID'")
     private String orgId;
 
-    @Column(nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "组织名称", required = true)
+    @Column(nullable = false, columnDefinition = "varchar(64) not null default '' comment '组织名称'")
     private String orgName;
 }
